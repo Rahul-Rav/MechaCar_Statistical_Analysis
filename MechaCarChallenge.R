b1ddf793a033404c0593943ee391e0981359d389
Rahul Raveendran <95504135+Rahul-Rav@users.noreply.github.com>
2022-04-07 22:30:13 -0400

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -7,4 +7,7 @@ library(dplyr)
 mechacar_mpg_df <- read.csv(file = "MechaCar_mpg.csv")
 
 # Perform linear regression using lm() function, pass in all six variables (columns) and add the created dataframe as the data parameter
-lm()
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechacar_mpg_df)
+
+# Using the summary() function, determine the p-value and the r-squared value for the linear regression model.
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = mechacar_mpg_df))
