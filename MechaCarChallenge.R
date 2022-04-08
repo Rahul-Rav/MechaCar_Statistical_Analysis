59d6e3cb379174ae27ddf26771c3c046ef730e15
Rahul Raveendran <95504135+Rahul-Rav@users.noreply.github.com>
2022-04-07 21:34:36 -0400

    Add files via upload

---
+++ MechaCarChallenge.R
@@ -0,0 +1,10 @@
+# Deliverable 1
+
+# Load 'dplyr' package using library() method
+library(dplyr)
+
+# Import and read MechaCar_mpg.csv file as a dataframe
+mechacar_mpg_df <- read.csv(file = "MechaCar_mpg.csv")
+
+# Perform linear regression using lm() function, pass in all six variables (columns) and add the created dataframe as the data parameter
+lm()
